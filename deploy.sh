0a77e360709dda57096beca821a5317036221ebf
Kenny Chou <kenny@kramxel.com>
2021-05-03 10:39:44 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# 當發生錯誤時終止腳本運行
+set -e
+# 打包
+yarn build
+# 移動至到打包後的dist目錄 
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/wbst-act/2021.git master:gh-pages
+cd -
