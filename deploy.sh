0c4d586bcb8e2d29d35495e1dc34b77cc0e26e0a
Kenny Chou <kenny@kramxel.com>
2021-05-03 10:56:31 +0800

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/wbst-act/2021.git master:gh-pages
+git push -f git@github.com:wbst-act/2021.git master:gh-pages
 cd -
