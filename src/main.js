738f97669e252774d11f2eb27bac6e19de58c9c1
Kenny Chou <kenny@kramxel.com>
2021-05-18 10:42:33 +0800

    add vue-gtag

--- src/main.js
+++ src/main.js
@@ -10,11 +10,13 @@ import VueMoment from 'vue-moment'
 import moment from 'moment-timezone'
 import VueOffline from 'vue-offline'
 import VueCookies from 'vue-cookies'
-
+import VueGtag from 'vue-gtag'
 
 require('moment/locale/zh-tw')
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, { config: { id: 'G-8MND1DTFLR' } }, router)
 Vue.use(VueAxios, axios)
 Vue.use(VueOffline)
 Vue.use(VueCookies)
